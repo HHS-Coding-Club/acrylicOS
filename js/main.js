42d8be4520dd4ed81ac57df1e800940f4b583968
Colack <UniformKitten281@gmail.com>
2024-07-18 11:17:37 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+    AcrylicOS
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    acrylic_system_bootSystem();
+    acrylic_desktop_createWindow('Welcome to AcrylicOS', 'This is a test window!');
+});
